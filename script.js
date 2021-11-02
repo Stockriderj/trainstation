c5a20b9a45935c0f80aa6a3955d58c22d2823024
Stockriderj <78283807+Stockriderj@users.noreply.github.com>
2021-11-01 20:20:39 -0500

    Sounds Update
    Each vehicle has their own sound, made sounds sound better, script cleanup, and other crazy stuff

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function getRandomNumber(min, max) {
 const passengerTrain = document.querySelector("#passenger-train");
 const cargoTrain = document.querySelector("#cargo-train");
 
-const trainSound1 = new Audio("sounds/train-1.wav");
+const passengerTrainSound = new Audio("sounds/passenger-train-1.wav");
+const cargoTrainSound = new Audio("sounds/cargo-train-1.wav")
 
 const vehicle1 = document.querySelector("#vehicle-1");
 
@@ -17,12 +18,12 @@ const busSound1 = new Audio("sounds/bus.wav");
 
 setInterval(() => {
     console.log("Passenger train is coming get off the trax");
-    trainSound1.play();
+    passengerTrainSound.play();
 
-	passengerTrain.style.background = "url('https://pluspng.com/img-png/trains-png-side-view-download-1000.png') center / contain no-repeat";
+	passengerTrain.style.backgroundImage = "url('https://pluspng.com/img-png/trains-png-side-view-download-1000.png')";
 	passengerTrain.style.right = "-100%";
 	setTimeout(() => {
-		passengerTrain.style.background = "none";
+		passengerTrain.style.backgroundImage = "none";
 		passengerTrain.style.right = "100%";
 	}, 1400);
 }, getRandomNumber(5000, 10000));
@@ -31,22 +32,22 @@ setInterval(() => {
     console.log("Vehicle 1 is coming get off the road");
     busSound1.play();
 
-	vehicle1.style.background = "url('https://pluspng.com/img-png/bus-png-side-view-bus-png-800.png') right / contain no-repeat";
+	vehicle1.style.backgroundImage = "url('https://pluspng.com/img-png/bus-png-side-view-bus-png-800.png')";
 	vehicle1.style.right = "100%";
 	setTimeout(() => {
-		vehicle1.style.background = "none";
+		vehicle1.style.backgroundImage = "none";
 		vehicle1.style.right = "-100%";
 	}, 10000);
 }, getRandomNumber(10000, 30000));
 
 setInterval(() => {
     console.log("Cargo train is coming get off the trax");
-    trainSound1.play();
+    cargoTrainSound.play();
 
-	cargoTrain.style.background = "url('images/cargo-train.png') center / cover no-repeat";
+	cargoTrain.style.backgroundImage = "url('images/cargo-train.png')";
 	cargoTrain.style.right = "-100%";
 	setTimeout(() => {
-		cargoTrain.style.background = "none";
+		cargoTrain.style.backgroundImage = "none";
 		cargoTrain.style.right = "100%";
 	}, 1000);
 }, getRandomNumber(5000, 20000));
